843c93e6148864731b9654ac66bc30a69a9a6030
yasserarnold <yasserarnold@yahoo.com>
2025-10-24 17:29:03 +0300

    Update persons.ts

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -10,7 +10,7 @@ export const persons: Person[] = [
 {
   id: "khaled-ibrahim",
   name: "خالد ابراهيم شقا",
-  image: "/images/khakhalledd.png",
+  image: "/images/khalledd.png",
   fallback: "أ",
   prayerAudio: "/audio/doaa.mp3"
 },
