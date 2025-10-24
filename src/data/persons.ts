40d6ad3d2466e6f54493fd9252a86efebd6c7c53
yasserarnold <yasserarnold@yahoo.com>
2025-10-24 19:37:13 +0300

    Add new person entry for الشيخ/ حسن عبد العظيم

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -55,6 +55,13 @@ prayerAudio: "/audio/doaa.mp3"
   image: "https://newoaks.s3.us-west-1.amazonaws.com/AutoDev/77020/3db4255c-464e-451f-a702-f7d8eb7fb1a5.png",
   fallback: "س"
 },
+
+	{
+  id: "hasan-abdelazeem",
+  name: "الشيخ/ حسن عبد العظيم",
+  image: "https://newoaks.s3.us-west-1.amazonaws.com/AutoDev/77020/3db4255c-464e-451f-a702-f7d8eb7fb1a5.png",
+  fallback: "س"
+},
 {
   id: "mahmoud-khalil-alhussary",
   name: "الشيخ/ محمود خليل الحصري",
